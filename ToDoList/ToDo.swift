fde893c953a92aa2a378419abeec8b21bfc6a010
Thomas Mullins <thomasmullinsdev@gmail.com>
2024-04-01 10:13:54 -0600

    Finished To-Do List App

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,49 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Thomas Mullins on 3/29/24.
+//
+
+import Foundation
+
+struct ToDo: Equatable, Codable {
+    let id: UUID
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("toDos").appendingPathExtension("plist")
+    
+    init(title: String, isComplete: Bool, dueDate: Date, notes: String?) {
+        self.id = UUID()
+        self.title = title
+        self.isComplete = isComplete
+        self.dueDate = dueDate
+        self.notes = notes
+    }
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        guard let codedToDos = try? Data(contentsOf: archiveURL) else { return nil }
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode(Array<ToDo>.self, from: codedToDos)
+    }
+    
+    static func saveToDos(_ toDos: [ToDo]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let codedToDos = try? propertyListEncoder.encode(toDos)
+        try? codedToDos?.write(to: archiveURL, options: .noFileProtection)
+        
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let toDo1 = ToDo(title: "Example To-Do", isComplete: false, dueDate: Date(), notes: "Example To-Do Notes")
+        return [toDo1]
+    }
+}
---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,35 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Thomas Mullins on 4/1/24.
+//
+
+import UIKit
+
+protocol ToDoCellDelegate: AnyObject {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    weak var delegate: ToDoCellDelegate?
+
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func completeButtonTapped(_ sender: Any) {
+        delegate?.checkmarkTapped(sender: self)
+    }
+}
